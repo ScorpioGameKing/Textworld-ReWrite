1434bbc138de1dd0d6af7ad4acc976c7531afa8a
ScorpioGK <scorpiogameking@gmail.com>
2026-04-22 13:29:27 -0400

    requirements, temp fonts and licences

--- textworld/_game.py
+++ textworld/_game.py
@@ -9,16 +9,14 @@ class TextworldGame():
     window: TextworldWindow
 
     def __init__(self):
-        #self.data_manager = DataManager()
+        self.data_manager = DataManager()
         self.window = TextworldWindow(Size(640,1280), "Textworld")
 
-        '''
-        world = TextworldWorld(Size(1, 1), Size(100, 100), 1)
+        world = TextworldWorld(Size(1, 1), Size(150, 150), 1)
         world.generate_world(self.data_manager["textworld"])
         for y in range(0, world.chunk_count.height):
                 for x in range(0, world.chunk_count.width):
                     world.dump_chunk(Coords(x, y))
-        '''
 
         self.window.create_window()
         self.window.add_to_render_pool("game_view", GameView(self.window))
--- textworld/ui/_fonts.py
+++ textworld/ui/_fonts.py
@@ -12,7 +12,12 @@ class Fonts():
 
     def load_fonts(self):
         for font in listdir(self._font_location):
-            self._fonts.update({font.split('.ttf')[0]:pr.load_font_ex(f"{self._font_location}{font}", 20, None, 0)})
-    
+            _font_split = font.split('.')
+            match _font_split[1]:
+                case 'ttf':
+                    self._fonts.update({_font_split[0]:pr.load_font_ex(f"{self._font_location}{font}", 20, None, 0)})
+                case 'otf':
+                    self._fonts.update({_font_split[0]:pr.load_font_ex(f"{self._font_location}{font}", 20, None, 0)})
+
     def __getitem__(self, font_name):
         return self._fonts[font_name]
